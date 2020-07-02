97468f2590db406067964924b5d020b9c918afa9
Wellington <wellingtonslpbi@hotmail.com>
2020-07-01 21:08:59 -0300

    create list and delete transactions waiting csv upload file

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,17 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column('timestamp')
   created_at: Date;
 
+  @Column('timestamp')
   updated_at: Date;
 }
 
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,38 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column('uuid')
   category_id: string;
 
+  @Column('timestamp')
   created_at: Date;
 
+  @Column('timestamp')
   updated_at: Date;
 }
 
